370e4db2dcef8dc471e5587d9c972681115a1cb2
John Wyles <john@fourstacks.co.uk>
2019-04-02 16:21:14 +0100

    Add filter component.  Needs work

---
+++ src/components/ListFilter/src/Filter.vue
@@ -0,0 +1,40 @@
+<script>
+
+	export default {
+
+		name: 'CtrlListFilter',
+
+		props: {
+			value: {
+				required: true,
+			}
+        },
+
+        methods:{
+			filterUpdated(value){
+				this.$emit("update", value)
+            }
+        },
+
+		render() {
+			return this.$scopedSlots.default({
+                // Data props
+				value: this.value,
+
+				filterProps: {
+					value: this.value
+				},
+
+                // Action props
+				filterUpdated: this.filterUpdated,
+
+                filterEvents: {
+					input: (e) =>  {
+						this.filterUpdated(e.target.value)
+					}
+                }
+			})
+		}
+
+	}
+</script>
