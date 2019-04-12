6ffb90883a3278da3474e677c637d477e908b61c
John Wyles <john@fourstacks.co.uk>
2019-04-12 07:17:58 +0100

    add dimensions component

---
+++ src/components/Dimensions/index.js
@@ -0,0 +1,7 @@
+import ControlDimensions from './src/Dimensions'
+
+ControlDimensions.install = function (Vue) {
+  Vue.component(ControlDimensions.name, ControlDimensions)
+}
+
+export default ControlDimensions
